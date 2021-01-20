8b37c25ea58c7915f8156424880f98abb0aaade6
turpp <white.tripp@icloud.com>
2021-01-20 12:52:34 -0600

    commit tree a99e4210b43293c2fac5e20ffd6512fccb78c787

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    let obj = {firstName: array[0], familyName: array[1], title: array[2], payPerHour: array[3], timeInEvents: [], timeOutEvents: []}
+    return obj
+}
+
+function createEmployeeRecords(array){
+    let arrObjs = array.map(employee => createEmployeeRecord(employee))
+    return arrObjs
+}
+
+function createTimeInEvent(timeStamp){
+   this.timeInEvents.push({type: 'TimeIn', hour: parseInt(timeStamp.split(' ')[1]), date: timeStamp.split(' ')[0]})
+    return this
+}
+
+function createTimeOutEvent(timeStamp){
+    this.timeOutEvents.push({type: 'TimeOut', hour: parseInt(timeStamp.split(' ')[1]), date: timeStamp.split(' ')[0]})
+    return this
+}
+
+function hoursWorkedOnDate(timeStamp){
+    let timeIn = this.timeInEvents.find(date => timeStamp == date.date)
+    let timeOut = this.timeOutEvents.find(date => timeStamp == date.date)
+    let hours = parseInt(timeOut.hour.toString().split('00')[0]) - parseInt(timeIn.hour.toString().split('00')[0])
+    return hours
+}
+
+function wagesEarnedOnDate(date){
+    let wagesOwed = hoursWorkedOnDate.call(this,date) * this.payPerHour
+    return wagesOwed
+}
+
+function findEmployeeByFirstName(array, name){
+    let result = array.find(record => record.firstName == name)
+    return result
+}
+
+function calculatePayroll(array){
+    let a = []
+    array.forEach(function(record){
+       a.push(allWagesFor.call(record))
+    })
+
+    let total = a.reduce(function(el, cv){
+        return el + cv 
+    })
+    console.log(total)
+    return total
+
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
